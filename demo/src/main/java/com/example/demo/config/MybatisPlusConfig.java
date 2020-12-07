32e81279d2e8557078d6b6178d1f3ab472198284
Afootly <3060660076@qq.com>
2020-12-07 10:45:47 +0800

    第一次提交到本地库

---
+++ demo/src/main/java/com/example/demo/config/MybatisPlusConfig.java
@@ -0,0 +1,11 @@
+package com.example.demo.config;
+
+/**
+ * MybatisPlus 相关配置
+ * @author www.dibo.ltd
+ * @version v1.0
+ * @date 2020/10/23
+ */
+public class MybatisPlusConfig {
+
+}
---
+++ demo/src/main/java/com/example/demo/test/hello.java
@@ -0,0 +1,9 @@
+package com.example.demo.test;
+
+public class hello {
+
+   public static void main(String[] args) {
+       System.out.println("更新1");
+
+    }
+}
