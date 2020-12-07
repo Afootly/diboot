8e26c60d3c41b36c3647d8a0da2819719cac4b3a
Afootly <65337832+Afootly@users.noreply.github.com>
2020-12-07 11:00:42 +0800

    更新本都库

--- demo/src/main/java/com/example/demo/test/hello.java
+++ demo/src/main/java/com/example/demo/test/hello.java
@@ -5,6 +5,8 @@ public class hello {
    public static void main(String[] args) {
        System.out.println("更新1");
        System.out.println("更新2");
+       System.out.println("更新本地库");
+
 
     }
 }
